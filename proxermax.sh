efa1092eb64d7cbc9ddbd92e78fc9e7759ac4c6e
KaliAjmal <114871423+CarlosMasio@users.noreply.github.com>
2025-05-07 07:18:14 -0400

    Update proxermax.sh

--- proxermax.sh
+++ proxermax.sh
@@ -6,14 +6,29 @@ windscribe start
 # Trap Ctrl+C (SIGINT) to disconnect Windscribe
 trap 'echo "Disconnecting Windscribe..."; windscribe disconnect; exit' INT
 
-# Infinite loop to change IP every 30 seconds
+# Clear the terminal
+clear
+
+# Print ASCII banner for PROXER-MAX
+echo -e "\e[1;34m"
+echo " _______  _______  _______           _______  _______         _______  _______          "
+echo "(  ____ )(  ____ )(  ___  )|\     /|(  ____ \(  ____ )       (       )(  ___  )|\     /|"
+echo "| (    )|| (    )|| (   ) |( \   / )| (    \/| (    )|       | () () || (   ) |( \   / )"
+echo "| (____)|| (____)|| |   | | \ (_) / | (__    | (____)| _____ | || || || (___) | \ (_) / "
+echo "|  _____)|     __)| |   | |  ) _ (  |  __)   |     __)(_____)| |(_)| ||  ___  |  ) _ (  "
+echo "| (      | (\ (   | |   | | / ( ) \ | (      | (\ (          | |   | || (   ) | / ( ) \ "
+echo "| )      | ) \ \__| (___) |( /   \ )| (____/\| ) \ \__       | )   ( || )   ( |( /   \ )"
+echo "|/       |/   \__/(_______)|/     \|(_______/|/   \__/       |/     \||/     \||/     \|"
+echo -e "\e[0m"
+
+# Author and GitHub info
+echo -e "             Created by \e[1m@ig.masio\e[0m"
+echo -e "  GitHub: \e[4mhttps://github.com/CarlosMasio/proxer-max.git\e[0m"
+echo
+
+# Start IP rotation every 30 seconds
 while true; do
-    # Disconnect current session if any
     windscribe disconnect
-
-    # Connect to a random location
     windscribe connect
-
-    # Wait for 30 seconds before changing IP again
     sleep 30
 done
